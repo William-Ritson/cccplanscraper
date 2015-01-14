7b33cf96e4d96958b9d116faa705919a6e8fa78b
unknown <william.ritson@gmail.com>
2015-01-14 15:54:05 -0800

    Added resume support and reduced memory usage
    Reduced memory usage by causing only one query to be stored
    in memory at a time. When a query completes the next query to be run
    is then stored on the disk to enable resumption.

--- scraping/assistScraper.js
+++ scraping/assistScraper.js
@@ -1,11 +1,15 @@
 var http = require('http'),
     request = require('request'),
     fs = require('fs'),
+    cli = require('cli'),
     dbin = require('../data/dbinput.js');
+
+var resumeFN = 'cache/assist/resume.json';
 
 var queryString = function (query) {
     var res = '?',
         first = true;
+
     query.forEach(function (item) {
         if (!first) {
             first = false;
@@ -14,18 +18,64 @@ var queryString = function (query) {
         }
         res += item[0] + '=' + item[1];
     });
+
     return res;
 };
 
-var respCount = 0;
-var runQuery = function (queries) {
-    var nextQ = queries.pop(),
-        major = nextQ.major,
-        toSchool = nextQ.to,
-        fromSchool = nextQ.from;
-    console.log('Query from', fromSchool, 'to', toSchool, 'in', major);
+var ids = JSON.parse(fs.readFileSync('./cache/assist/assist-index.json'));
+console.log('ids', 'to:', ids.to.length, 'from: ', ids.from.length);
 
-    var query = [
+var incrementQuery = function (query, first) {
+    var limits = [
+        ['from', ids.from[query.count.from].length],
+        ['major', ids.to[query.count.to].major.length],
+        ['to', ids.to[query.count.to].length],
+        ['end', 1]
+    ];
+    if (first !== true) {
+        query.count.from += 1;
+    }
+    query.done += 1;
+
+    limits.forEach(function (limit, index) {
+        if (query.count[limit[0]] >= limit[1]) {
+            query.count[limit[0]] = 0;
+            query.count[limits[index + 1][0]]++;
+        }
+    });
+
+    if (query.count.end > 0) {
+        return false;
+    }
+    return true;
+};
+
+/// Make it so it only stores one query at a time
+var buildQuery = function (query) {
+    query.next = {
+        to: ids.to[query.count.to],
+        from: ids.from[query.count.from],
+        major: ids.to[query.count.to].major[query.count.major],
+        end: 0
+    };
+    return query;
+};
+
+var runQuery = function (query) {
+    var nextQ = query.next,
+        major = nextQ.major.id,
+        toSchool = nextQ.to.id,
+        fromSchool = nextQ.from.id;
+
+    // clear the console
+    process.stdout.write('\033c');
+
+
+    cli.progress(query.done / query.total);
+
+    console.log('Query', fromSchool, 'to', toSchool, 'major', major, '(', query.done, '/', query.total, ')');
+
+    var options = [
         ['aay', '13-14'],
         ['dora', major],
         ['oia', toSchool],
@@ -42,82 +92,72 @@ var runQuery = function (queries) {
         ['kind', 5],
         ['dt', 2]
     ],
-        url = 'http://web1.assist.org/cgi-bin/REPORT_2/Rep2.pl' + queryString(query);
+        url = 'http://web1.assist.org/cgi-bin/REPORT_2/Rep2.pl' + queryString(options);
 
 
     request(url, function (error, response, body) {
+        console.log('query done');
         if (!error) {
-            respCount++;
-            var fn = 'scraped/' + fromSchool + 'to' + toSchool + 'in' + major + '.html';
+            var fn = 'cache/assist/agreements/' + fromSchool + 'to' + toSchool + 'in' + major + '.html';
+
             console.log('ok', fromSchool, 'to', toSchool, 'major', major, 'STATUS: ' + response.statusCode);
 
-            fs.writeFileSync(fn, body);
-            dbin.storeAgreement({
-                from: fromSchool,
-                to: toSchool,
-                major: major,
+            var data = {
+                from: nextQ.from,
+                to: nextQ.to,
+                major: nextQ.major,
                 source: body,
                 srcUrl: url,
                 scrapedDate: new Date()
-            });
+            };
+
+            fs.writeFileSync('cache/assist/agreements/' + fromSchool + 'to' + toSchool + 'in' + major + '.json',
+                JSON.stringify(data));
+
+
+            //         dbin.storeAgreement();
 
         } else {
             console.log('error', fromSchool, 'to', toSchool, 'major', major, 'STATUS: ' + error);
         }
-        console.log('starting next query remainig', queries.length);
-        if (queries.length > 0) {
-            runQuery(queries);
+
+        var next = incrementQuery(query, false);
+        fs.writeFileSync('cache/assist/resume.json', JSON.stringify(query));
+
+        if (next) {
+            runQuery(buildQuery(query));
         }
     });
 };
 
-var fromSchools = [
-        'WVC', // West Valley College
-        'HARTNELL',
-        'DAC', //De Anza
-        'MONTEREY',
-        'CABRILLO'
-    ],
-    toSchools = [
-        {
-            id: 'UCD',
-            title: 'University of California Davis',
-            majors: ['MATH.B.S', 'BIOLSCI.B.S.', 'HIST.A.B.', 'ENGLISH.A.B.', 'CHEM.B.S.', 'PHYSICS.B.S.', 'COMP.SCI.B.S.', 'ENG.CIV.B.S.', 'GEOLOGY.B.S', 'ECON.A.B.', 'POL.SCI.A.B.', 'PHILOS.A.B.', 'SOCIOL.A.B.', 'PSYCH.A.B.', 'INTREL']
-        },
-        {
-            id: 'UCSC',
-            title: 'University of California Santa Cruz',
-            majors: ['MATH', 'BIOL', 'HIS', 'CHEM', 'PHYS', 'CMPSBS', 'EE', 'ECON', 'POLI', 'PHIL', 'SOCY', 'PSYC', 'BME']
-        },
-        {
-            id: 'UCB',
-            title: 'University of Califronia Berkly',
-            majors: ['MATH', 'CHEMBIO', 'HISTORY', 'ENGLISH', 'CHEM.AB.BS ', 'PHYSICS', 'CS-AB', 'EECS', 'ECON', 'POL-SCI', 'PHILOS', 'SOCIOL', 'PSYCH', 'BUS ADM']
-        },
-        {
-            id: 'UCSD',
-            title: 'University of California San Diego',
-            majors: ['MATHEMATICS', 'GENERAL BIOLOGY', 'HISTORY', 'CHEMISTRY', 'PHYSICS B.S.', 'COMP SCI', 'ELEC ENGR', 'ECONOMICS', 'POLITICAL SCIENCE', 'PHILOSOPHY', 'SOCIOLOGY', 'PSYCHOLOGY']
-        }
-    ];
 
-var buildQuires = function () {
-    var queries = [];
-    toSchools.forEach(function (toSchool) {
-        toSchool.majors.forEach(function (major) {
-            fromSchools.forEach(function (fromSchool) {
-                queries.push({
-                    to: toSchool.id,
-                    from: fromSchool,
-                    major: major
-                });
-
-            });
-        });
+var getTotal = function () {
+    var count = 0;
+    ids.to.forEach(function (to) {
+        console.log(count, to.major.length);
+        count += to.major.length;
     });
-    return queries;
+    count *= ids.from.length;
+    return count;
 };
 
 module.exports.run = function () {
-    runQuery(buildQuires());
+    var query;
+    if (fs.existsSync(resumeFN)) {
+        query = JSON.parse(fs.readFileSync(resumeFN));
+    } else {
+        query = {
+            count: {
+                to: 0,
+                from: 0,
+                major: 0
+            },
+            done: 0,
+            total: getTotal(),
+            next: {}
+        };
+        incrementQuery(query, true);
+    }
+    runQuery(buildQuery(query));
 };
+module.exports.run();
